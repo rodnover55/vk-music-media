4eb19632f8f1cf7f397f5ee3d6512a164f3beeb6
Sergei Melnikov <me@rnr.name>
2016-11-26 23:43:39 +0300

    Added post item getting request.

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -16,24 +16,33 @@ class PostController extends Controller
     public function index() {
         $posts = Post::with(['tags', 'tracks'])->get();
 
-
         return new JsonResponse($this->transformPosts($posts));
+    }
+
+    public function show($id) {
+        $post = Post::with(['tags', 'tracks'])->findOrFail($id);
+
+        return new JsonResponse($this->transformPost($post));
     }
 
     public function transformPosts(Collection $posts) {
         $data = $posts->map(function (Post $item) {
-            $tags = $item->tags->pluck('tag')->toArray();
-
-            $post = $item->attributesToArray();
-            $post['tracks'] = $item->tracks->map(function (Track $track) {
-                return $track->attributesToArray();
-            })->toArray();
-
-            $post['tags'] = $tags;
-
-            return $post;
+            return $this->transformPost($item);
         })->toArray();
 
         return $data;
+    }
+
+    public function transformPost(Post $item) {
+        $tags = $item->tags->pluck('tag')->toArray();
+
+        $post = $item->attributesToArray();
+        $post['tracks'] = $item->tracks->map(function (Track $track) {
+            return $track->attributesToArray();
+        })->toArray();
+
+        $post['tags'] = $tags;
+
+        return $post;
     }
 }
--- backend/tests/PostTest.php
+++ backend/tests/PostTest.php
@@ -43,4 +43,29 @@ class PostTest extends TestCase
         $this->assertCount($post->tags->count(), $json[0]['tags']);
         $this->assertCount($post->tracks->count(), $json[0]['tracks']);
     }
+
+    public function testPostItem() {
+        $object = $this->loadYmlFixture('get-posts.yml');
+
+        $postId = $object['post']->id;
+        $this->auth($object['token']->token)->getJson("/api/posts/{$postId}");
+
+        $this->assertResponseOk();
+
+        $json = $this->decodeResponseJson();
+
+        $this->seeJsonStructure([
+            'id', 'pid', 'group_id', 'created_at', 'title', 'image', 'description',
+            'created_at', 'updated_at',
+            'tags' => [
+                '*'=> []
+            ],
+            'tracks' => [
+                '*' => [
+                    'id', 'aid', 'owner_id', 'title', 'artist', 'duration',
+                    'created_at', 'updated_at'
+                ]
+            ]
+        ], $json);
+    }
 }
