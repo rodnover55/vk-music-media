58512dc2773710a7f1451e37ad09cfb004db3374
Sergei Melnikov <me@rnr.name>
2016-11-26 10:41:11 +0300

    Changed app name.

--- backend/app/Http/Controllers/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Auth/RegisterController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace VkMusic\Http\Controllers\Auth;
 
-use App\User;
+use VkMusic\User;
 use Validator;
-use App\Http\Controllers\Controller;
+use VkMusic\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
